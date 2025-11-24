46c62768084e6b33868ee1cf6406c3d451041c61
David Peicho <david.peicho@wonderlandengine.com>
2025-11-24 16:58:19 +0100

    interaction: Fix grab/release for pivoting objects

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,6 @@ import { vec3 } from "gl-matrix";
 /** Epsilon floating point value. */
 export const EPSILON = 0.000001;
 
+export const RIGHT = vec3.set(vec3.create(), 1, 0, 0);
 export const UP = vec3.set(vec3.create(), 0, 1, 0);
 export const FORWARD = vec3.set(vec3.create(), 0, 0, -1);
